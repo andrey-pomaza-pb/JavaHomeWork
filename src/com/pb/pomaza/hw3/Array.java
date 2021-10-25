2176f0da56a8d333818730720ddffeda899aeab9
Andrey <andpom25@gmail.com>
2021-10-25 15:25:45 +0300

    init commit

--- src/com/pb/pomaza/hw3/Array.java
+++ src/com/pb/pomaza/hw3/Array.java
@@ -1,42 +1,59 @@
 package com.pb.pomaza.hw3;
 
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Array {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner input = new Scanner(System.in);
-            System.out.println("внесите 10 значный массив ");
-            int mas[] = new int[10];
-            for (int i = 0; i < mas.length; i++) {
-                mas[i] = input.nextInt();
-            }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Заполните массив");
+        int mas[] = new int[10];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = input.nextInt();
+        }
+        System.out.println("Заполненый массив");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");// Выводим заполненый массив на экран
+        }
+        System.out.println();
+
+        int sum = 0;
+        for (int i = 0; i < mas.length; i++) {
+            sum = sum + mas[i];
+        }
+        System.out.println("сумма всех элементов =  " + sum);
 
 
-            System.out.println("Внесенный  массив");
-            for (int i=0;i < mas.length;i++){
-                System.out.print(mas[i]+ " ");
-            }
-            System.out.println();
+
+        System.out.println("массив перед пузырьковой сортировкой : " + Arrays.toString(mas));
+
+
+        bubbleSort(mas);
+
+
+        System.out.println("массив после пузырьковой сортировки : " + Arrays.toString(mas));
+    }
 
 
 
-            int num = 0;
-            for (int i=0 ; i< mas.length; i++){
-                if (mas[i]>0 ){
-                    num++;
+    public static void bubbleSort(int[] num) {
+        int j;
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+            for (j = 0; j < num.length - 1; j++) {
+                if (num[j] > num[j + 1]) {
+                    temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                    flag = true;
                 }
-
-
             }
-            System.out.println("Количество положительных значений  =  "+ num);
-
-            int sum = 0;
-            for (int i= 0; i < mas.length; i++) {
-                sum = sum + mas[i];
-            }
-            System.out.println("общая сумма значений =  " + sum);
-
         }
     }
 
+}
--- src/com/pb/pomaza/hw3/Bingo.java
+++ src/com/pb/pomaza/hw3/Bingo.java
@@ -1,28 +1,39 @@
 package com.pb.pomaza.hw3;
 
+
+import java.util.Random;
 import java.util.Scanner;
+
 public class Bingo {
     public static void main(String[] args) {
-        int UnknownNumber;
-        int UserNumber;
-        int TrysCount = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Задание: Угадать целое число от 0 до 100.");
-        System.out.println("для выхода введите  home");
-        UnknownNumber = (68);
-        do {
-            TrysCount++;
-            System.out.print("Введите предпологаемое число  ");
-            UserNumber = input.nextInt();
-            if (UserNumber > UnknownNumber)
-                System.out.println("холодно");
-            else if (UserNumber < UnknownNumber)
-                System.out.println("гарячевато)");
-            else System.out.println("BINGOO!");
-            if (input.equals("home")) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int rum = random.nextInt(101);
+        int count = 0;
+        System.out.println("Угадайте число от 0 до 100");
+        System.out.println("для выхода введите home");
+        while(true){
+            System.out.print("Введите число:  ");
+            String data = sc.next();
+            if (data.equals("home")){
                 break;
             }
-        } while (UserNumber != UnknownNumber);
-        System.out.println("попыток за игру" + TrysCount);
+            int num = Integer.parseInt(data);
+            count++;
+            if (num != rum){
+                if (num < rum){
+                    System.out.println(num + " < ?");
+                }
+                else {
+                    System.out.println(num + " > ?");
+                }
+            }
+            else {
+                System.out.println("Вы победили!!!  " + num + ". число попыток - " + count);
+                break;
+            }
+
+        }
+        System.out.println("Игра окончена !");
     }
 }
